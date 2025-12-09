4406e0ab29b7877655d4e9aa2af3b83634c61626
c3230214 <c3230214@std.gku.ac.jp>
2025-12-09 15:24:43 +0900

    Update inventory.js

--- daisho-day4/assets/js/inventory.js
+++ daisho-day4/assets/js/inventory.js
@@ -1,4 +1,5 @@
-// ▼ 在庫データ（デフォルト値：localStorageに何もないとき用）let items = [
+// ▼ 在庫データ（デフォルト値：localStorageに何もないとき用）
+ let items = [
   { id: "D001", name: "オレンジジュース", price: 150, stock: 20, minStock: 5 },
   { id: "D002", name: "コーラ",           price: 150, stock: 12, minStock: 5 },
   { id: "D003", name: "お茶",             price: 120, stock: 30, minStock: 10 }
